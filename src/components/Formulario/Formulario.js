9c1e817f804ed6a406a7a0b0aa500a5408c8d56c
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-04 18:07:27 -0300

    escultando evento submit e desfazendo o comportamento padrao do button

--- src/components/Formulario/Formulario.js
+++ src/components/Formulario/Formulario.js
@@ -15,14 +15,19 @@ const Formulario = () => {
         'Inovação e Gestão'
     ]
 
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido')
+    }
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome"></CampoTexto>
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo"></CampoTexto>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"></CampoTexto>
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo"></CampoTexto>
                 <CampoTexto label="Imagem" placeholder="Digite o endereco da imagem"></CampoTexto>
-                <ListaSuspensa label="Time" itens={times}></ListaSuspensa>
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}></ListaSuspensa>
                 <Botao>Criar Card</Botao>
             </form>
         </section>
