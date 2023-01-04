d772b9e0f80856630d9035e1b4d04efee9e49d3f
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-04 18:31:21 -0300

    manipulando input

--- src/components/CampoTexto/CampoTexto.js
+++ src/components/CampoTexto/CampoTexto.js
@@ -2,10 +2,17 @@ import './CampoTexto.css'
 
 const CampoTexto = (props) => {
     //console.log(props)
+
+    let valor = 'Lucas'
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(evento.target.value)
+    }
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}></input>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}></input>
         </div>
     )
 }
