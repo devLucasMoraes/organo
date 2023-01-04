75effb073d751476dcb9e95b668befda791416ab
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-04 16:16:56 -0300

    criado componente: CampoTexto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Banner from './components/Banner/Banner';
+import CampoTexto from './components/CampoTexto/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner></Banner>
+      <CampoTexto label="Nome"></CampoTexto>
+      <CampoTexto label="Cargo"></CampoTexto>
+      <CampoTexto label="Imagem"></CampoTexto>
     </div>
   );
 }
