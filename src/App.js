8e14e2d78d3277f88f50ee37c2b672529a14b6be
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-04 16:57:09 -0300

    criado componente: Formulario

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Banner from './components/Banner/Banner';
-import CampoTexto from './components/CampoTexto/CampoTexto';
+import Formulario from './components/Formulario/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner></Banner>
-      <CampoTexto label="Nome" placeholder="Digite seu nome"></CampoTexto>
-      <CampoTexto label="Cargo" placeholder="Digite seu cargo"></CampoTexto>
-      <CampoTexto label="Imagem" placeholder="Digite o endereco da imagem"></CampoTexto>
+      <Formulario></Formulario>
+
     </div>
   );
 }
