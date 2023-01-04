a1a29f5d23355cf38f2ca8d60d406210353ac7c5
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-04 19:44:02 -0300

    cadastrando colaboradores

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import Banner from './components/Banner/Banner';
 import Formulario from './components/Formulario/Formulario';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorCadastrado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
       <Banner></Banner>
-      <Formulario></Formulario>
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorCadastrado(colaborador)}></Formulario>
 
     </div>
   );
