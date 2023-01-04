269f7b070a06f48f1d08ca648190b2bc6ae0f605
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-04 15:24:45 -0300

    primeiro componente: Banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX
+    return (
+        <header className='banner'>
+            <img src="/images/banner.png" alt="Banner principal"></img>
+        </header>
+    )
+}
+
+export default Banner
