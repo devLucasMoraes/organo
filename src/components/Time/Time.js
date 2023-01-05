cdbecb835ed2e9f269f162ac4ccccff1cae2dee9
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-05 17:17:40 -0300

    criando um estado inicial

--- src/components/Time/Time.js
+++ src/components/Time/Time.js
@@ -8,13 +8,16 @@ const Time = (props) => {
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
             <div className='colaboradores'>
                 {props.colaboradores
-                    .map(colaborador => <Colaborador 
-                                            key={colaborador.nome}
-                                            nome={colaborador.nome} 
-                                            cargo={colaborador.cargo} 
-                                            img={colaborador.imagem}
-                                            corPrimaria={props.corPrimaria}>
-                                        </Colaborador>)}
+                    .map(colaborador => {
+                        //console.log(<Colaborador key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} img={colaborador.imagem} corPrimaria={props.corPrimaria}></Colaborador>)
+                        return <Colaborador 
+                        key={colaborador.nome}
+                        nome={colaborador.nome} 
+                        cargo={colaborador.cargo} 
+                        img={colaborador.imagem}
+                        corPrimaria={props.corPrimaria}>
+                    </Colaborador>
+                    })}
             </div>
         </section>
         : ''
