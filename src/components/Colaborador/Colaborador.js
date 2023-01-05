8ae1cb9923e492288d307d77bedcd65b3905021d
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-04 21:50:48 -0300

    criado componente: Colaborador

---
+++ src/components/Colaborador/Colaborador.js
@@ -0,0 +1,18 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='http://github.com/devlucasmoraes.png' alt='Lucas Moraes'></img>
+            </div>
+            <div className='rodape'>
+                <h4>Lucas Moraes</h4>
+                <h5>Desenvolvedor</h5>
+            </div>
+        </div>
+        )
+}
+
+export default Colaborador
